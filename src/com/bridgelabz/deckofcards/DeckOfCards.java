fbc62a74c2a849f3715743bd413d221ee0a58089
Prashanth <imprash4u@gmail.com>
2021-09-13 08:56:43 +0530

    Added: Welcome message To Deck of Cards program

---
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.deckofcards;
+
+public class DeckOfCards {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deck of Cards Program");
+    }
+}
