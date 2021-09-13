0a84d14dc70579792a0c6cf4bdb9a799df843f16
Prashanth <imprash4u@gmail.com>
2021-09-13 10:00:41 +0530

    Added:Number of players Taking from Console

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,30 +1,46 @@
 package com.bridgelabz.deckofcards;
 
+import java.util.Scanner;
+
 public class DeckOfCards {
-    public static void deckOfcards(){
-        String[] suits = {"Clubs", "Daimonds", "Hearts","Spades"};
-        String[] ranks = {"2", "3", "4","5","6","7","8","9","10","Jack","King","Queen","Ace"};
+    Scanner scanner = new Scanner(System.in);
+
+    public void deckOfcards() {
+        String[] suits = {"Clubs", "Daimonds", "Hearts", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
 
         int sizeOfDeck = suits.length * ranks.length;
-        System.out.println("Number of cards in the Deck:"+sizeOfDeck);
+        System.out.println("Number of cards in the Deck:" + sizeOfDeck);
         String[] deck = new String[sizeOfDeck];
-        for( int i = 0; i< ranks.length; i++){
-            for(int j=0; j<suits.length; j++){
-                deck[suits.length*i+j]=ranks[i] +"->" +suits[j];
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                deck[suits.length * i + j] = ranks[i] + "->" + suits[j];
             }
         }
         toPrint(deck);
     }
 
-    private static void toPrint(String[] deck) {
+    private void toPrint(String[] deck) {
         System.out.println("Cards in Deck:");
-        for(String element : deck){
+        for (String element : deck) {
             System.out.print("\t" + element);
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Deck of Cards Program");
-        DeckOfCards.deckOfcards();
+        DeckOfCards deck = new DeckOfCards();
+        deck.deckOfcards();
+        deck.players();
+    }
+
+    private void players() {
+        System.out.println("Enter number of players minimum 2 , maximum 4");
+        int player = scanner.nextInt();
+        if (player >= 2 && player <= 4) {
+            System.out.println(player + " will players will  play the game");
+        } else {
+            System.out.println("please enter number of players in the Range");
+        }
     }
 }
